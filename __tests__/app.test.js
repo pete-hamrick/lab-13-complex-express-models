f35424efe9a8ce46eccc59fb32719f47bd592a07
Pete Hamrick <pete.hamrick@gmail.com>
2021-10-04 14:04:39 -0700

    passing species test; setup app, controller, model and sql to get passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,21 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('Species routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('should let you add a new Species', () => {
+    return request(app)
+      .post('/api/species')
+      .send({ type: 'Mammal' })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '1',
+          type: 'Mammal',
+        });
+      });
   });
 
   afterAll(() => {
