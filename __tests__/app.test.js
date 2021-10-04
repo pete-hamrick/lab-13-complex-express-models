daf7d851e0e197997836146a2c09cdc0981e81b9
Pete Hamrick <pete.hamrick@gmail.com>
2021-10-04 14:41:40 -0700

    hackery passing get all function, needs refactoring

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,32 +3,48 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-async function saveSomeSpecies() {
-  request(app).post('/api/species').send({ type: 'Mammal' });
-  request(app).post('/api/species').send({ type: 'Amphibian' });
-  request(app).post('/api/species').send({ type: 'Reptile' });
-}
+// async function saveSomeSpecies() {
+//   const mammal = { type: 'Mammal' };
+//   const amphibian = { type: 'Amphibian' };
+//   const reptile = { type: 'Reptile' };
+
+//   Promise.all([mammal, amphibian, reptile]);
+//   request(app).post('/api/species').send({ type: 'Mammal' });
+//   request(app).post('/api/species').send({ type: 'Amphibian' });
+//   request(app).post('/api/species').send({ type: 'Reptile' });
+// }
 
 describe('Species routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  beforeEach(() => {
+    return request(app).post('/api/species').send({ type: 'Mammal' });
+  });
+  beforeEach(() => {
+    return request(app).post('/api/species').send({ type: 'Amphibian' });
+  });
+  beforeEach(() => {
+    return request(app).post('/api/species').send({ type: 'Reptile' });
+  });
 
   it('should let you add a new Species', () => {
     return request(app)
       .post('/api/species')
-      .send({ type: 'Mammal' })
+      .send({ type: 'Bird' })
       .then((res) => {
         expect(res.body).toEqual({
-          id: '1',
-          type: 'Mammal',
+          id: '4',
+          type: 'Bird',
         });
       });
   });
 
   it('should get all species', async () => {
-    await saveSomeSpecies();
-
+    // await saveSomeSpecies();
+    // request(app).post('/api/species').send({ type: 'Mammal' });
+    // request(app).post('/api/species').send({ type: 'Amphibian' });
+    // request(app).post('/api/species').send({ type: 'Reptile' });
     return request(app)
       .get('/api/species')
       .then((res) => {
