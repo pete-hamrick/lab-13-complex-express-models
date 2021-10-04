cff01a84ef2cdeee088e14184e4d87def3d83526
Pete Hamrick <pete.hamrick@gmail.com>
2021-10-04 14:13:41 -0700

    wrote failing get all species test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,12 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+async function saveSomeSpecies() {
+  request(app).post('/api/species').send({ type: 'Mammal' });
+  request(app).post('/api/species').send({ type: 'Amphibian' });
+  request(app).post('/api/species').send({ type: 'Reptile' });
+}
+
 describe('Species routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -17,6 +23,29 @@ describe('Species routes', () => {
           id: '1',
           type: 'Mammal',
         });
+      });
+  });
+
+  it('should get all species', async () => {
+    await saveSomeSpecies();
+
+    return request(app)
+      .get('/api/species')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: '1',
+            type: 'Mammal',
+          },
+          {
+            id: '2',
+            type: 'Amphibian',
+          },
+          {
+            id: '3',
+            type: 'Reptile',
+          },
+        ]);
       });
   });
 
